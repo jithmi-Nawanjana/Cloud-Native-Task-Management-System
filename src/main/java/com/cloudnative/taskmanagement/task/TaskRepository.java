0dda8843fd4a0a7da61c69b644c42a721287ab0a
Jithmi Nawanjana <jithminawa@gmail.com>
2025-12-09 18:17:12 +0530

    implemented query methods for filtering

--- src/main/java/com/cloudnative/taskmanagement/task/TaskRepository.java
+++ src/main/java/com/cloudnative/taskmanagement/task/TaskRepository.java
@@ -1,8 +1,15 @@
 package com.cloudnative.taskmanagement.task;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+
+    List<Task> findByStatus(Task.Status status);
+
+    List<Task> findByPriority(Task.Priority priority);
+
+    List<Task> findByAssignee_Id(Long assigneeId);
 }
 
